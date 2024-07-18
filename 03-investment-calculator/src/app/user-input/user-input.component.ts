9c78c5689efd66b4befd5d7edfee50e5f2db4b88
Manuel Beltran <manuel.beltranc@hotmail.com>
2024-07-17 18:40:48 -0700

    userInputComponent finished and investmentResultComponenet started

--- 03-investment-calculator/src/app/user-input/user-input.component.ts
+++ 03-investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
 
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,     // Number(string) +string hacen lo mismo
+      duration: +this.enteredDuration,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn
+    })
+  }
 }
