67910eb35cc4f80d6db6dde5e59328892a4229a9
Manuel Beltran <manuel.beltranc@hotmail.com>
2024-07-18 18:39:29 -0700

    minor format edit

--- 03-investment-calculator-module/src/app/user-input/user-input.module.ts
+++ 03-investment-calculator-module/src/app/user-input/user-input.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from "@angular/core";
-import { UserInputComponent } from "./user-input.component";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { UserInputComponent } from './user-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [UserInputComponent],
-    imports: [FormsModule],
-    exports: [UserInputComponent]
+  declarations: [UserInputComponent],
+  imports: [FormsModule],
+  exports: [UserInputComponent],
 })
-export class UserInputModule {
-
-}
+export class UserInputModule {}
